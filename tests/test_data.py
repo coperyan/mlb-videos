9f1dc58a433f9cd04dfa47a9fb02f28002fdde9f
Ryan Cope <rcope@1800radiator.com>
2022-05-11 11:00:58 -0700

    Update tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,7 +2,7 @@ import os
 import logging
 import logging.config
 
-os.chdir(os.path.join(os.path.expanduser('~'),'Desktop/Dev/mlb_videos/mlb_videos'))
+#os.chdir('mlb_videos')
 
 #Import modules needed
 from sources.statcast import Statcast
@@ -14,44 +14,22 @@ logging.config.fileConfig('logging.ini')
 logger = logging.getLogger(__name__)
 
 #Search date
-DATE = '2022-05-04'
+START_DATE = '2022-05-04'
 
 #Get pitches from yesterday
 #Backing up statcast for easy cache?
-sc_df = Statcast(start_dt = DATE, end_dt = DATE).get_df()
+sc_df = Statcast(start_dt = START_DATE, end_dt = START_DATE).get_df()
 sc_df.to_csv('data/statcast.csv',index=False)
 
 #Init pitch collection from data grabbed (essentially just a class with a DF)
 p = Pitches(sc_df)
-
-#Calculating missed calls
 p.calculate_missed_calls()
-
-#Adding criteria, >= 3" strike or >= 4" ball
-p.df = p.df[
-    ((p.df['total_miss'] >= 3.00) & (p.df['description'] == 'called_strike')) |
-    ((p.df['total_miss'] >= 4.00) & (p.df['description'] == 'ball'))
-]
-
-#Adding rank of worst misses
 p.rank_pitches(
     partition_by = [],
     order_by = 'total_miss',
     ascending = False,
     name = 'total_miss_rank'
 )
-
-#Adding rank of worst change in run expectancy
-p.rank_pitches(
-    partition_by = [],
-    order_by = 'delta_run_exp',
-    ascending = True,
-    name = 'total_run_exp_chg_rank'
-)
-
-#Get videos
-p.get_videos()
-
 
 # p.rank_pitches(
 #     partition_by = ['game_date'], order_by = 'total_miss',
@@ -62,11 +40,11 @@ p.get_videos()
 # p.df.to_csv('data/misses.csv',index=False)
 
 
-# p.df = p.df[
-#     ((p.df['total_miss'] >= 3) & (p.df['description'] == 'called_strike')) |
-#     ((p.df['total_miss'] >= 4) & (p.df['description'] == 'ball')) |
-#     (p.df['total_miss_rank'] <= 35)
-# ]
+p.df = p.df[
+    ((p.df['total_miss'] >= 3) & (p.df['description'] == 'called_strike')) |
+    ((p.df['total_miss'] >= 4) & (p.df['description'] == 'ball')) |
+    (p.df['total_miss_rank'] <= 35)
+]
 
 #p.df = p.df.sort_values(by='total_miss_rank',ascending=True).reset_index(drop=True)
 
