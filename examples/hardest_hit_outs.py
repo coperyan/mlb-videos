5c633942d7fbc14edde66c61bd92b3f222eef721
Ryan Cope <ryancope2009@gmail.com>
2023-08-17 12:30:29 +0000

    updt

--- examples/hardest_hit_outs.py
+++ examples/hardest_hit_outs.py
@@ -1,25 +1,58 @@
 import os
+import logging
+import logging.config
+from mlb_videos.util import setup_project
 
-from data import Data
-from video import Compilation
+_PROJECT_NAME = "test"
+project_path = setup_project(_PROJECT_NAME)
 
-_PROJECT_NAME = "hardest_hit_outs"
+logging.config.fileConfig("logging.ini", defaults={"project_name": _PROJECT_NAME})
+logger = logging.getLogger(__name__)
 
-mlbdata = Data(
-    project_name=_PROJECT_NAME, start_date="2023-03-01", end_date="2023-11-30"
-)
+from mlb_videos import MLBVideoClient
 
-mlbdata.query_df("description == 'hit_into_play'")
-mlbdata.query_df("events not in ('single','double','triple','home_run')")
-mlbdata.rank_df(
-    name="exit_velocity_rank", group_by=None, fields=["launch_speed"], ascending=[False]
-)
-mlbdata.query_df("exit_velocity_rank <= 20")
-mlbdata.get_videos(download=True)
-
-comp = Compilation(
-    name=_PROJECT_NAME,
-    df=mlbdata.df,
-    local_path=mlbdata.local_path,
-    metric_caption="launch_speed",
+test = MLBVideoClient(
+    project_name=_PROJECT_NAME,
+    project_path=project_path,
+    start_date="2023-03-01",
+    end_date="2023-08-16",
+    enable_cache=False,
+    game_info=False,
+    player_info=False,
+    team_info=False,
+    analysis=False,
+    queries=None,
+    steps=[
+        {"type": "query", "params": {"query": "events == 'home_run'"}},
+        {
+            "type": "query",
+            "params": {
+                "query": "(home_team == 'SF' & inning_topbot == 'Bot') | (away_team == 'SF' & inning_topbot == 'Top')"
+            },
+        },
+        {
+            "type": "rank",
+            "params": {
+                "name": "distance_rank",
+                "group_by": None,
+                "fields": ["hit_distance_sc"],
+                "ascending": [False],
+                "keep_sort": True,
+            },
+        },
+        {"type": "query", "params": {"query": "distance_rank <= 30"}},
+    ],
+    search_clips=False,
+    download_clips=True,
+    build_compilation=True,
+    compilation_params={"metric_caption": "hit_distance_sc"},
+    upload_youtube=True,
+    youtube_params={
+        "title": "San Francisco Giants | Longest Home Runs (MLB 2023)",
+        "description": "Here are the top 30 Giants home runs from this year so far.",
+        "tags": ["longest home runs", "furthest home runs", "dingers"],
+        "playlist": "Longest Home Runs",
+        "thumbnail": "resources/jd_davis_batflip.jpg",
+    },
+    purge_files=False,
 )
